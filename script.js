f2eb183e9e076bc1429dd8d93efb2e97c7ea9510
Isa Coronel <156938989+icoronel1@users.noreply.github.com>
2024-03-27 15:57:18 +0000

    mostly complete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
  Project 2 - Array of Objects
- Name: 
+ Name: Isa Coronel
  Comments: 
  */
 
@@ -11,16 +11,55 @@
 ***/
 
 // Global Variables go here
+//var sprite1, sprite2;
+var sprites = new Array(10);
 
 function setup(){
   // this function will run once
   createCanvas(600, 400); // create a 600 x 400 pixel drawing canvas
-
+  
+  for(let i = 0; i < sprites.length; i++){ //conditions for 'for loop.' initalize, compare and if true increase i by 1
+    sprites[i] = new Sprite(random(width), random(height));
+    console.log(sprites[i]);
+  }
 
 }
 
 function draw(){
   background(200); //light gray background
+  for(let i = 0; i < sprites.length; i++){
+    sprites[i].display();
+    sprites[i].move();
+  }
   
 }
 
+function Sprite (tempX, tempY){
+  this.x = tempX;
+  this.y = tempY;
+  this.xSpeed = random(-2, 2);
+  this.ySpeed = random(-2, 2);
+
+  this.move = function(){
+    this.x += this.xSpeed
+    this.y += this.ySpeed
+
+    if(this.x > width || this.x < 0){
+      this.xSpeed *= -1;
+    }
+
+    if(this.y > height || this.y < 0){
+      this.ySpeed *= -1;
+    }
+  }
+
+  this.display = function(){
+    push(); // new layer
+    translate(this.x, this.y) // move layer's origin point to 0,0
+    rectMode(CENTER) // center rectangle
+    fill(255, 0, 0, 50) // transparent red
+    rect(0, 0, 75); // rectangle
+    pop(); // dispose of layer
+  }
+
+}
